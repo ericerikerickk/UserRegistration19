0a0f64f7f3d329ac9af4014b5bee90948b4e600c
Eric Poblete <113760743+ericerikerickk@users.noreply.github.com>
2023-03-19 21:49:16 +0800

    031923949PM

--- BorrowerForm.cs
+++ BorrowerForm.cs
@@ -23,7 +23,7 @@ namespace UserRegistration19
         {
             con.Open();
 
-            SqlCommand cmd = new SqlCommand("Select fname AS [First Name], lname AS [Last Name], address AS [Address], contact AS [Contact Number] from users order by Id asc", con);
+            SqlCommand cmd = new SqlCommand("Select Id AS [ID],  fname AS [First Name], lname AS [Last Name], address AS [Address], contact AS [Contact Number] from users order by Id asc", con);
             cmd.ExecuteNonQuery();
 
             SqlDataAdapter adap = new SqlDataAdapter(cmd);
@@ -38,14 +38,54 @@ namespace UserRegistration19
         private void btnAdd_Click(object sender, EventArgs e)
         {
             con.Open();
-            long contact = long.Parse(txtContact.Text);
+            SqlCommand checkID = new SqlCommand("select ID from users where ID='" + int.Parse(txtID.Text) + "'", con);
+            SqlCommand checkFname = new SqlCommand("select fname from users where fname='" + txtFname.Text + "'", con);
+            SqlDataAdapter sd = new SqlDataAdapter(checkID);
+            DataTable dt = new DataTable();
+            sd.Fill(dt);
+            SqlDataAdapter sdFname = new SqlDataAdapter(checkFname);
+            DataTable dtFname = new DataTable();
+            sdFname.Fill(dtFname);
+            // Checking Last Name
+            SqlCommand checkLname = new SqlCommand("select lname from users where lname='" + txtLname.Text + "'", con);
+            SqlDataAdapter sdLname = new SqlDataAdapter(checkLname);
+            DataTable dtLname = new DataTable();
+            sdLname.Fill(dtLname);
+            // Checking Contact
+            SqlCommand checkContact = new SqlCommand("select contact from users where contact='" + long.Parse(txtContact.Text) + "'", con);
+            SqlDataAdapter sdContact = new SqlDataAdapter(checkContact);
+            DataTable dtContact = new DataTable();
+            sdContact.Fill(dtContact);
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("ID already Exist!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                con.Close();
+            }
+            else if(dtFname.Rows.Count > 0)
+            {
+                MessageBox.Show("First Name already Exist!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                con.Close();
+            }
+            else if (dtLname.Rows.Count > 0)
+            {
+                MessageBox.Show("Last Name already Exist!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                con.Close();
+            }
+            else if (dtContact.Rows.Count > 0)
+            {
+                MessageBox.Show("Contact Number already Exist!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                con.Close();
+            }
+            else
+            {
+                long contact = long.Parse(txtContact.Text);
 
-            SqlCommand cmd = new SqlCommand("Insert into users values ('" + txtID.Text + "', '" + txtFname.Text + "', '" + txtLname.Text + "', '" + txtAddress.Text + "', '" + contact + "')", con);
-            cmd.ExecuteNonQuery();
-            MessageBox.Show("Successfully Saved!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            con.Close();
-            loadDataGrid();
-            
+                SqlCommand cmd = new SqlCommand("Insert into users values ('" + txtID.Text + "', '" + txtFname.Text + "', '" + txtLname.Text + "', '" + txtAddress.Text + "', '" + contact + "')", con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully Saved!", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                con.Close();
+                loadDataGrid();
+            }         
         }
 
         private void btnEdit_Click(object sender, EventArgs e)
@@ -85,6 +125,7 @@ namespace UserRegistration19
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            txtID.Text = dataGridView1.Rows[e.RowIndex].Cells["ID"].Value.ToString();
             txtFname.Text = dataGridView1.Rows[e.RowIndex].Cells["First Name"].Value.ToString();
             txtLname.Text = dataGridView1.Rows[e.RowIndex].Cells["Last Name"].Value.ToString();
             txtAddress.Text = dataGridView1.Rows[e.RowIndex].Cells["Address"].Value.ToString();
